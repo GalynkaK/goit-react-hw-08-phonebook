1c151d03a6f742fb4f9ff967e6c3b60435c2f7fe
GalynkaK <gkarpinskaya@gmail.com>
2023-06-25 21:28:35 +0300

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
-import { lazy, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { useAuth } from "./Hooks/useAuth";
-import { refreshUser } from "../redux/auth/operations";
-import Loader from "./Loader/Loader";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Layout } from "./Layout";
-import { RestrictedRoute } from "./RestrictedRoute";
-import { PrivateRoute } from "./PrivateRoute";
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useAuth } from './Hooks/useAuth';
+import { refreshUser } from '../redux/auth/operations';
+import Loader from './Loader/Loader';
+import { Layout } from './Layout';
+import { RestrictedRoute } from './RestrictedRoute';
+import { PrivateRoute } from './PrivateRoute';
+import { Route, Routes } from 'react-router-dom';
+import { lazy } from 'react';
+
 const HomePage = lazy(() => import('../pages/Home.js'));
 const RegisterPage = lazy(() => import('../pages/Register.js'));
 const LoginPage = lazy(() => import('../pages/Login.js'));
@@ -14,7 +16,6 @@ const ContactsPage = lazy(() => import('../pages/Contacts.js'));
 
 const App = () => {
   const dispatch = useDispatch();
-
   const { isRefreshing } = useAuth();
 
   useEffect(() => {
@@ -24,34 +25,35 @@ const App = () => {
   return isRefreshing ? (
     <Loader />
   ) : (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route
-            path="/register"
-            element={
-              <RestrictedRoute
-                redirectTo="/contacts"
-                component={<RegisterPage />}
-              />
-            }
-          />
-          <Route
-            path="/login"
-            element={
-              <RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />
-            }
-          />
-          <Route
-            path="/contacts"
-            element={
-              <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
-            }
-          />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route
+          path="register"
+          element={
+            <RestrictedRoute
+              redirectTo="/contacts"
+              component={<RegisterPage />}
+            />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute
+              redirectTo="/contacts"
+              component={<LoginPage />}
+            />
+          }
+        />
+        <Route
+          path="/contacts"
+          element={
+            <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+          }
+        />
+      </Route>
+    </Routes>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom';
 import App from './components/App';
 import './index.css';
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import store, { persistor } from './redux/store';
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
